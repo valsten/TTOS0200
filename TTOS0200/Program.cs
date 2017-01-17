2e8a9f01c714e60f842e07c6c0d4bb0aa482fc3e
K2480 <K2480@student.jamk.fi>
2017-01-17 17:14:22 +0200

    Reformatted comments

--- TTOS0200/Program.cs
+++ TTOS0200/Program.cs
@@ -4,12 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
- * Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) 
- * vastaavan luvun sanana (yksi, kaksi tai kolme). Jos käyttäjä syöttää 
- * jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku". 
- */
-
 namespace TTOS0200
 {
     class Program
@@ -21,6 +15,13 @@ namespace TTOS0200
 
         static void Teht1()
         {
+
+            /*
+            * Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) 
+            * vastaavan luvun sanana (yksi, kaksi tai kolme). Jos käyttäjä syöttää 
+            * jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku". 
+            */
+
             Console.Write("Write a number (1, 2 or 3): ");
 
             string tempnumber = Console.ReadLine();
@@ -41,6 +42,11 @@ namespace TTOS0200
 
         static void Teht2()
         {
+            /*
+             * Tee ohjelma, jossa annetaan oppilaalle koulunumero seuraavan 
+             * taulukon mukaan (pistemäärä kysytään ja ohjelma tulostaa numeron):
+             */ 
+
             Console.Write("Input your score and I'll tell you your grade: ");
 
             string tempscore = Console.ReadLine();
