b658489b5d986d71a1a0cceff29aceaa6c5fa6be
Valtteri Stenholm <Valtteri Stenholm>
2017-01-30 20:33:24 +0200

    Finished assignemnt 3.5 and 3.6.

---
+++ Homework/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Vehicle
+    {
+
+        public Vehicle(string name, int speed, int tyres)
+        {
+            Name = name;
+            Speed = speed;
+            Tyres = tyres;
+        }
+
+        // Properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        // Methods
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle: " + Name);
+            Console.WriteLine("Speed: " + Speed);
+            Console.WriteLine("Tyres: " + Tyres);
+        }
+
+        public override string ToString()
+        {
+            string s = Name + ", " + Speed + ", " + Tyres;
+            return s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle0 = new Homework.Vehicle("Toyota", 60, 12);
+            Vehicle vehicle1 = new Homework.Vehicle("Fiat", 45, 15);
+            Vehicle vehicle2 = new Homework.Vehicle("Skoda", 55, 20);
+            Vehicle vehicle3 = new Homework.Vehicle("Subaru", 80, 14);
+
+            vehicle0.PrintData();
+            vehicle1.PrintData();
+            vehicle2.PrintData();
+            vehicle3.PrintData();
+
+            Console.WriteLine("Data in one string: ");
+            Console.WriteLine(vehicle0.ToString());
+            Console.WriteLine(vehicle1.ToString());
+            Console.WriteLine(vehicle2.ToString());
+            Console.WriteLine(vehicle3.ToString());
+
+        }
+    }
+}
---
+++ Homework3.5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3._5
+{
+    class Student
+    {
+        public string Number { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+
+        public Student(string number, string firstname, string surname)
+        {
+            Number = number;
+            Firstname = firstname;
+            Surname = surname;
+        }
+
+        public override string ToString()
+        {
+            string s = Number + ", " + Firstname + ", " + Surname;
+            return s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student("123", "John", "Doe");
+            students[1] = new Student("777", "Jane", "Doe");
+            students[2] = new Student("321", "Tommy", "Atkins");
+            students[3] = new Student("971", "Karl", "Marx");
+            students[4] = new Student("414", "Rosa", "Luxemburg");
+
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(students[i].ToString());
+            }
+        }
+    }
+}
