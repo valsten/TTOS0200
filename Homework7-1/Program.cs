a752d64e4ddef9e52de0204de0d3f77937ce63eb
Valtteri Stenholm <K2480@Labranet.jamk.fi>
2017-02-21 15:51:05 +0200

    Finished assignment 7-1

---
+++ Homework7-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"D:/K2480/teksti1.txt");
+                while (true)
+                {
+                    Console.WriteLine("Enter text: ");
+                    var text = Console.ReadLine();
+                    if (text.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                        break;
+                    outputFile.WriteLine(text);
+                }
+                outputFile.Close();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Your input wasn't valid!");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Some error occured.");
+            }
+        }
+    }
+}
