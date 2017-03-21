f0716dbd36e054ec57e8336182ce532fc75acdfb
K2480 <K2480@TIET-331-WS-32>
2017-03-21 13:15:36 +0200

    Finished WPF stuff.

---
+++ WPFtesti/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFtesti
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int count1 = 0;
+        int count2 = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            count1++;
+            counter1.Text = count1.ToString();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            count2++;
+            counter2.Text = count2.ToString();
+        }
+    }
+}
---
+++ wpfteht2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfteht2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void eurotomarkka_Click(object sender, RoutedEventArgs e)
+        {
+            double temp = Int32.Parse(amount.Text);
+            double conversion = temp * 5.94573;
+            converted.Text = conversion.ToString("0.00");
+        }
+
+        private void markkatoeuro_Click(object sender, RoutedEventArgs e)
+        {
+            double temp = Int32.Parse(amount.Text);
+            double conversion = temp / 5.94573;
+            converted.Text = conversion.ToString("0.00");
+        }
+    }
+}
